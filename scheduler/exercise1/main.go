05c110695041f1ad7e4aff764b262a3226e762bd
chenjj <cjj9931207@gmail.com>
2020-02-13 14:09:57 +0800

    1

---
+++ scheduler/exercise1/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main(){
+	runtime.GOMAXPROCS(1)
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	a := func(){
+		for i:= 100;i>0;i--{
+			fmt.Println("a:",i)
+		}
+		wg.Done()
+	}
+	b := func(){
+		for i:=0;i<100;i++{
+			fmt.Println("b:",i)
+		}
+		wg.Done()
+	}
+	go a()
+	go b()
+	wg.Wait()
+}
